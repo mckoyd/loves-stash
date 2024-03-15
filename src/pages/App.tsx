822ea1e3c0104e04af344a07b9a54ef601636bfb
mckoyd <dominick.mckoy@gmail.com>
2024-03-14 22:20:52 -0700

    adds Homepage endpoint

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,27 +1,17 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 
 import "../styles/App.css";
+import Homepage from "./Homepage";
 
 const App: React.FC = () => {
-  return <section className="app-section">Hey Beautiful!</section>;
+  return (
+    <section className="app-section">
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+      </Routes>
+    </section>
+  );
 };
 
 export default App;
-
-// const App: React.FC = () => {
-//     const screenSize = useScreenSizeUpdate();
-
-//     return (
-//       <section className="app-section">
-//         {screenSize <= layout.mobile && (
-//           <BgSidebarMobile className="bg-sidebar-mobile" />
-//         )}
-//         <Routes>
-//           <Route path="/" element={<PersonalInfo />} />
-//           <Route path="/select-your-plan" element={<SelectYourPlan />} />
-//           <Route path="/add-ons" element={<h2>Add-ons</h2>} />
-//           <Route path="/finish" element={<h2>Finish</h2>} />
-//         </Routes>
-//       </section>
-//     );
-//   };
---
+++ src/pages/Homepage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Homepage: React.FC = () => {
+  return (
+    <section className="homepage">
+      <header className="header">
+        <h1>Love's Stash</h1>
+      </header>
+    </section>
+  );
+};
+
+export default Homepage;
